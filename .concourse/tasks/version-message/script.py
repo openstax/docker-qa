a6dc73590f09fa9a19281b508dbd83c8c6439a4f
m1yag1 <mike.arbelaez@rice.edu>
2020-04-22 13:51:17 -0500

    updated script to use build metadata

--- .concourse/tasks/version-message/script.py
+++ .concourse/tasks/version-message/script.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python
-import json
 import os
 
 from string import Template
 
-ATC_EXTERNAL_URL = os.environ.get("ATC_EXTERNAL_URL")
-BUILD_TEAM_NAME = os.environ.get("BUILD_TEAM_NAME")
-BUILD_PIPELINE_NAME = os.environ.get("BUILD_PIPELINE_NAME")
-BUILD_JOB_NAME = os.environ.get("BUILD_JOB_NAME")
-BUILD_NAME = os.environ.get("BUILD_NAME")
 
-def build_concourse_url():
-    return f"{ATC_EXTERNAL_URL}/teams/{BUILD_TEAM_NAME}/pipelines/" \
-           f"{BUILD_PIPELINE_NAME}/jobs/{BUILD_JOB_NAME}/builds/" \
-           f"{BUILD_NAME or BUILD_PIPELINE_NAME or BUILD_JOB_NAME}"
+def read_file(filename):
+    with open(filename, "r") as infile:
+        data = infile.read()
+    return data
 
 
-message_template = """:female-detective: A new version of chromedriver $version 
-has been detected. Builds have been started in Concourse. Watch the build status
-here: $build_url
+def build_concourse_build_url(atc_external_url,
+                              built_team_name,
+                              build_pipeline_name,
+                              build_job_name,
+                              build_name):
+    return f"{atc_external_url}/teams/{build_team_name}/pipelines/{build_pipeline_name}/jobs/{build_job_name}/builds/{build_name}"
+
+
+message_template = """:female-detective: A new version of chromedriver $version has been detected. 
+Builds have been started in Concourse. Check $build_url for status.
 """
 
 template = Template(message_template)
 
-with open("./chromedriver-latest/version", "r") as infile:
-    chromedriver_version = infile.read()
+chromedriver_version = read_file("./chromedriver-latest/version")
+
+# Read in all the concourse build meta into variables
+atc_external_url = read_file("./meta/atc-external-url")
+build_team_name = read_file("./meta/build-team-name")
+build_pipeline_name = read_file("./meta/build-pipeline-name")
+build_job_name = read_file("./meta/build-job-name")
+build_name = read_file("./meta/build-name")
 
 print(
     template.substitute(
         version=chromedriver_version,
-        build_url=build_concourse_url(),
+        build_url=build_concourse_build_url(atc_external_url, 
+                                            build_team_name, 
+                                            build_pipeline_name,
+                                            build_job_name,
+                                            build_name)
     )
 )
